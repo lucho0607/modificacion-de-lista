d0f5eb18d176039b37d070729506c957765f4ad4
luis <luisfcs791@gmail.com>
2023-10-02 21:51:36 -0500

    edicion para lista de contactos

---
+++ contactos/app.js
@@ -0,0 +1,9 @@
+const contactos = [
+    {nombre: "luis", apellido: "cubillos"},
+    {nombre: "gabriella", apellido: "cubillos"},
+    {nombre: "javier", apellido: "cubillos"},
+    {nombre: "oscar", apellido: "cubillos"},
+    {nombre: "maria", apellido: "cubillos"}
+];
+
+console.log(contactos);
